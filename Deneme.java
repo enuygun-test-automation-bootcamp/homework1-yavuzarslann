bb0c8963835fd42d5d21a6fcc9c5a032c53cb4ae
yavuzarslann <yavuz-arslan98@hotmail.com>
2022-06-22 20:53:33 +0300

    ikinci commit

--- Deneme.java
+++ Deneme.java
@@ -1,5 +1,6 @@
-public class deneme {
+public class Deneme {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("merhaba dünya");
     }
 }
